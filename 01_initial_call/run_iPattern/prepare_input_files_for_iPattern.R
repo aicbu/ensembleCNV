df404c8c2cbda494267b9378e5324e0e29b5a262
Diyanath Ranasinghe <81305927+aicbu@users.noreply.github.com>
2023-04-11 16:58:19 +0100

    custom edit

--- 01_initial_call/run_iPattern/prepare_input_files_for_iPattern.R
+++ 01_initial_call/run_iPattern/prepare_input_files_for_iPattern.R
@@ -1,27 +1,29 @@
 #!/urs/bin/env Rscript
 
+## Edited to generate the inputs in a custom folder (not the ensembleCNV folder structure)
 ## The script was used to prepare auxiliary input files for iPattern
 ## the auxiliary input files will be stored at ${WKDIR}/01_initial_call/run_iPattern/data_aux
+## run the script in the same folder as gender file
+
+# make a new dir for output
+mkdir("data_aux")
 
 args <- commandArgs( trailingOnly = TRUE )
 
-## working directory
-path_wkdir <- args[1]
+## add sig intensity file path
+path_sig <- args[1]
 
-## project name for running iPattern
+## project name for running iPattern - not needed
 project_name <- args[2]
-
-## path to the directory for running iPattern
-path_run_ipattern <- file.path(path_wkdir, "01_initial_call", "run_iPattern")
 
 ##--------------------------------------------------------------------------------
 ## 1) data_file: list of splitted final report files for each sample
 ## the directory contains the input files prepared by finalreport_to_iPattern.pl
-path_ipattern_prepare_data <- file.path(path_run_ipattern, "data")
+path_ipattern_prepare_data <- file.path(path_sig)
 fls_all <- list.files(path = path_ipattern_prepare_data, pattern = ".txt$", full.names = TRUE)
 
 data_file <- data.frame(data_file = fls_all, stringsAsFactors = FALSE)
-write.table( data_file, file = file.path( path_run_ipattern, "data_aux", 
+write.table( data_file, file = file.path("data_aux", 
                                           paste0(project_name, "_data_file.txt")),
              sep = "\t", row.names = FALSE, col.names = FALSE, quote = FALSE)
 
@@ -36,9 +38,9 @@ write.table( data_file, file = file.path( path_run_ipattern, "data_aux",
 # Sample_2	F
 # Sample_3	F
 
-gender_file <- read.delim(file = file.path(path_wkdir, "data", "Samples_Table.txt"), as.is = TRUE)
+gender_file <- read.delim(file = file.path("Samples_Table.txt"), as.is = TRUE)
 gender_file$Gender <- toupper( substr(gender_file$Gender, 1, 1) )
-write.table( gender_file, file = file.path( path_run_ipattern, "data_aux", 
+write.table( gender_file, file = file.path("data_aux", 
                                             paste0(project_name, "_gender_file.txt")),
              sep = "\t", row.names = FALSE, col.names = FALSE, quote = FALSE)
 
@@ -50,13 +52,13 @@ write.table( gender_file, file = file.path( path_run_ipattern, "data_aux",
 # bad_sample_3
 
 ## We prepare an empty file. The user can type in bad samples.
-write.table(NULL, file = file.path( path_run_ipattern, "data_aux",
+write.table(NULL, file = file.path("data_aux",
                                     paste0(project_name, "_bad_samples.txt")),
             sep = "\t", row.names = FALSE, col.names = FALSE, quote = FALSE)
 
 cat("Processing is completed.\n")
 cat("Three files are generated:\n")
-cat(file.path( path_run_ipattern, "data_aux", paste0(project_name, "_data_file.txt")), "\n")
-cat(file.path( path_run_ipattern, "data_aux", paste0(project_name, "_gender_file.txt")), "\n")
-cat(file.path( path_run_ipattern, "data_aux", paste0(project_name, "_bad_samples.txt")), "\n")
+cat(file.path("data_aux", paste0(project_name, "_data_file.txt")), "\n")
+cat(file.path("data_aux", paste0(project_name, "_gender_file.txt")), "\n")
+cat(file.path("data_aux", paste0(project_name, "_bad_samples.txt")), "\n")
 
